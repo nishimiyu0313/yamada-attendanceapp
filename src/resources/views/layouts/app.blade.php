1ff543b6a2f64539bbf70a3b0090b70cc6c3d807
miyu <yookmiyu@kvision.ne.jp>
2025-10-04 21:43:57 +0900

    リポジトリの変更

---
+++ src/resources/views/layouts/app.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>coachteckattendance</title>
+    <link rel="stylesheet" href="{{ asset('css/common.css')}}">
+    <link rel="stylesheet" href="{{ asset('css/sanitize.css')}}">
+    @yield('css')
+</head>
+
+<body>
+
+
+
+
+
+
+
+
+
+
+</body>
+
+</html>
